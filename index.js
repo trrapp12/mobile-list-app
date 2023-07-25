4a39014e75b57138081fd7ad2ae0ebcd05d3089e
Trevor Rapp <11747875+trrapp12@users.noreply.github.com>
2023-07-25 14:09:18 -0600

    adds onValue to grab current real time items from Firebase, still have to fix list displaying twice

--- index.js
+++ index.js
@@ -1,27 +1,30 @@
 import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js'
-import { getDatabase, ref, push } from 'https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js'
+import { getDatabase, ref, push, onValue } from 'https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js'
+import { displayInput, resetInputField, displayList} from './utils/util-functions.js'
 
 // setup Firebase app
 const appSettings ={
     databaseURL : 'https://mobile-list-app-default-rtdb.firebaseio.com/'
-}
+};
 
-const app = initializeApp(appSettings)
-const database = getDatabase(app)
-const itemsInListInDB = ref(database, 'items')
+const app = initializeApp(appSettings);
+const database = getDatabase(app);
+const itemsInListInDB = ref(database, 'items');
 
-console.log(app)
+onValue(itemsInListInDB, (snapshot) => {
+    resetInputField(inputFieldEl);
+    let itemsArray = Object.values(snapshot.val())
+    itemsArray.map(x => displayList(shoppingListContainer, x))
+})
+// console.log(app);
 
 // set up constants
-const inputFieldEl = document.getElementById('input-field')
-const addButtonEl = document.getElementById('add-button')
-const shoppingListContainer = document.querySelector('#shopping-list')
+const inputFieldEl = document.getElementById('input-field');
+const addButtonEl = document.getElementById('add-button');
+const shoppingListContainer = document.querySelector('#shopping-list');
 
 addButtonEl.addEventListener('click', function() {
-    let inputValue = inputFieldEl.value
-    
-    push(itemsInListInDB, inputValue)
-    console.log(`${inputValue} added to database`)
-
-    shoppingListContainer.innerHTML += `<li>${inputValue}</li>`
+    let inputValue = displayInput(inputFieldEl);
+    push(itemsInListInDB, inputValue);
+    console.log(`${inputValue} added to database`);
 })
---
+++ utils/util-functions.js
@@ -0,0 +1,11 @@
+export function displayInput(el) {
+    return el.value;
+}
+
+export function resetInputField(el) {
+    el.value = ''
+}
+
+export function displayList (el, value) {
+    el.innerHTML += `<li>${value}</li>`
+}
